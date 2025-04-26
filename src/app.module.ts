8088e754fa8d5c3d807bab14cde9ee3235d9fd5e
Umar <umar.dev500@gmail.com>
2025-04-26 15:12:25 +0700

    use .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule, ProductModule } from 'src/interface/http/modules';
 
 @Module({
-  imports: [ProductModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    ProductModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/interface/http/modules/auth.module.ts
+++ src/interface/http/modules/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthUseCase } from 'src/app/usecase';
@@ -8,11 +9,15 @@ import { AuthController } from 'src/interface/http/controllers';
   controllers: [AuthController],
   imports: [
     PassportModule,
-    JwtModule.register({
-      secret: 'secret',
-      signOptions: {
-        expiresIn: '1h',
-      },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN'),
+        },
+      }),
     }),
   ],
   providers: [AuthUseCase],
