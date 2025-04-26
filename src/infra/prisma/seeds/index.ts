a35b3c8859158039b741d08bbbd319227771e70b
Umar <umar.dev500@gmail.com>
2025-04-26 15:31:15 +0700

    add user seed

--- src/infra/prisma/seeds/index.ts
+++ src/infra/prisma/seeds/index.ts
@@ -1,11 +1,13 @@
 import seedCategories from 'src/infra/prisma/seeds/category.seed';
 import seedMerchant from 'src/infra/prisma/seeds/merchant.seed';
 import seedProducts from 'src/infra/prisma/seeds/product.seed';
+import seedUser from 'src/infra/prisma/seeds/user.seed';
 import seedVariants from 'src/infra/prisma/seeds/variant.seed';
 
 async function main() {
   console.log('🌱 Starting seeding...');
   await seedMerchant();
+  await seedUser();
   await seedCategories();
   await seedVariants();
   await seedProducts();
---
+++ src/infra/prisma/seeds/user.seed.ts
@@ -0,0 +1,33 @@
+import * as bcrypt from 'bcrypt';
+import { PrismaClient } from 'prisma/generated';
+
+const prisma = new PrismaClient();
+
+export default async function seedUser() {
+  console.log('🌟 Seeding users...');
+
+  const users = [
+    {
+      fullname: 'John Doe',
+      email: 'H2M2A@example.com',
+      password: 'password123', // Plain text password
+      merchant_id: 1,
+    },
+  ];
+
+  // Loop through each user and hash the password before creating
+  for (const user of users) {
+    // Hash the password before saving it to the database
+    const hashedPassword = await bcrypt.hash(user.password, 10); // 10 is the salt rounds
+
+    // Create the user with the hashed password
+    await prisma.user.create({
+      data: {
+        ...user,
+        password: hashedPassword, // Save the hashed password
+      },
+    });
+  }
+
+  console.log('✅ Seeding complete!');
+}
